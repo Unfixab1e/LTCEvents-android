b87f85d9dd026eaeb3ab813ee0aa5dd29f03fa27
Unfixab1e <michael.vogl@outlook.com>
2017-03-25 12:56:07 +0900

    Merge branch 'master' of http://github.com/LearnTeachCodeSeoul/LTCEvents-android
    # Conflicts:
    #	app/src/main/java/org/learnteachcode/seoul/android/ltcevents/DisplayMessageActivity.java
    #	app/src/main/java/org/learnteachcode/seoul/android/ltcevents/MainActivity.java
    #	app/src/main/res/layout/activity_display_message.xml
    #	app/src/main/res/layout/activity_main.xml
    #	app/src/main/res/values/strings.xml

--- app/src/main/java/org/learnteachcode/seoul/android/ltcevents/DisplayMessageActivity.java
+++ app/src/main/java/org/learnteachcode/seoul/android/ltcevents/DisplayMessageActivity.java
@@ -1,8 +1,8 @@
 package org.learnteachcode.seoul.android.ltcevents;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
 public class DisplayMessageActivity extends AppCompatActivity {
